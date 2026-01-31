423134bca3f84cd0fd168abf1aea0d3221da4071
muhammed-asharaf-vp <mohamedasharafvpp@gmail.com>
2026-01-31 14:16:34 +0530

    ERP

--- components/features/booking/BookingForm.tsx
+++ components/features/booking/BookingForm.tsx
@@ -40,14 +40,23 @@ export default function BookingForm() {
   return (
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <SenderSection />
-        <ReceiverSection />
-        <PackageSection />
+  {/* Sender + Receiver (2-column on desktop) */}
+  <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+    <SenderSection />
+    <ReceiverSection />
+  </div>
 
-       <div className="flex justify-end">
-  <SubmitButton />
-</div>
-      </form>
+  {/* Package Details below */}
+  <div className="mt-6">
+    <PackageSection />
+  </div>
+
+  {/* Submit button */}
+  <div className="mt-6 flex justify-end">
+    <SubmitButton />
+  </div>
+</form>
+
     </FormProvider>
   );
 }
